179a39cf5413d671390e26ba0c3d9b0b86bf2a96
小白 <1274464354@qq.com>
2018-01-14 10:05:28 +0800

    init

---
+++ src/main/java/com/xb/informall/InformallApplication.java
@@ -0,0 +1,12 @@
+package com.xb.informall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InformallApplication.class, args);
+	}
+}
